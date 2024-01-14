6f0e1218bc932b834ac79fc83597e10eb647cabb
Gulnar Hagverdiyeva <glnrhagverdiyeva@gmail.com>
2024-01-14 21:37:06 +0400

    Initial commit

--- index.js
+++ index.js
@@ -54,30 +54,76 @@
 
 
 
-const first = (value)=>{
-return new Promise((resolve , reject)=>{
-    if(value){
-        resolve("resolve edildi")
-    }
+// const first = (value)=>{
+// return new Promise((resolve , reject)=>{
+//     if(value){
+//         resolve("resolve edildi")
+//     }
 
-    else{
-        reject("reject edildi")
-    }
-})
+//     else{
+//         reject("reject edildi")
+//     }
+// })
+// }
+
+// const irst = first(true)
+
+// irst.then((data)=>{
+//     console.log("data" , data);
+//     return first(true)
+// }).catch((err)=>{
+//     console.log("Error" , err);
+// }).then((data)=>{
+//     console.log("data2" , data);
+//     return first(false)
+// }).then((data)=>{
+//     console.log(data);
+// }).catch((data)=>{
+//     console.log("day besdi" , data);
+// })
+
+
+async function  getData(){
+    // const response = await fetch("https://northwind.vercel.app/api/products")
+    // const products = await response.json();
+    // const catagoriesResponse = await fetch("https://northwind.vercel.app/api/categories")
+
+    // const catagories = await catagoriesResponse.json()
+    // console.log(products , catagories);
+const response = await
+    Promise.all([
+        fetch("https://northwind.vercel.app/api/products"),
+        fetch("https://northwind.vercel.app/api/categories"),
+    ])
+
+    const bodyResponses = await Promise.all([
+        response[0].json(),
+        response[1].json(),
+    ])
+
+    console.log(bodyResponses);
 }
 
-const irst = first(true)
+getData();
 
-irst.then((data)=>{
-    console.log("data" , data);
-    return first(true)
-}).catch((err)=>{
-    console.log("Error" , err);
-}).then((data)=>{
-    console.log("data2" , data);
-    return first(false)
-}).then((data)=>{
-    console.log(data);
-}).catch((data)=>{
-    console.log("day besdi" , data);
-})
+// fetch("https://northwind.vercel.app/api/products").then( async (response)=>{
+//     // console.log(response);
+// //    let body = await response.json()
+// //    console.log(body);
+
+// return response.json();
+// }).then((data)=>{
+//     console.log(data);
+// })
+ 
+
+// fetch("https://northwind.vercel.app/api/categories").then( async (response)=>{
+//     // console.log(response);
+// //    let body = await response.json()
+// //    console.log(body);
+
+// return response.json();
+// }).then((data)=>{
+//     console.log(data);
+// })
+ 
